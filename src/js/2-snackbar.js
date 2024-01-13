aac77110149427578906da3b258a4d21ebc0a550
Merylly <142231027+Merylly@users.noreply.github.com>
2024-01-13 16:58:59 +0200

    add js tasks

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,33 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const stateValue = form.elements.state.value;
+  const delay = form.elements.delay.value;
+
+  createPromise(delay, stateValue)
+    .then(delay =>
+      iziToast.success({ message: `Fulfilled promise in ${delay}ms` })
+    )
+    .catch(delay =>
+      iziToast.error({ message: `Rejected promise in ${delay}ms` })
+    );
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+console.log(1);
